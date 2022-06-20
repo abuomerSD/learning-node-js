0f92ce5c28555b9f6d4e2ee66177ccf7f3cda0f7
EltayebIbrahim <tayeb1293@yahoo.com>
2022-06-19 21:15:47 -0700

    commit

--- app.js
+++ app.js
@@ -4,5 +4,19 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req,res)=>{
-    res.send();
+    // res.send('<p> home page </p>');
+    res.sendFile('./views/index.html', {root: __dirname});
+});
+
+app.get('/about', (req,res)=>{
+    res.sendFile('./views/about.html', {root : __dirname})
+});
+
+app.get('/about-us', (req, res)=>{
+    res.redirect('/about');
+});
+
+//404 page 
+app.use((req,res)=>{
+    res.status(404).sendFile('./views/404.html', {root: __dirname});
 });
